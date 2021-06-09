972fd5c3b1945ae304f7eeef85ca81d63e5fc8e6
Pinky Rout <pinkyrout@Pinkys-MacBook-Pro.local>
2021-06-09 18:34:56 +0530

    [3-create-new-note-ui] - Added select dropdown for tags and contact while adding new note. - Added due date input with switch in add new note form. - Added sample data for contacts list and added constant for tags list.

--- app/javascript/src/components/Dashboard/Notes/NewNoteForm.jsx
+++ app/javascript/src/components/Dashboard/Notes/NewNoteForm.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import * as yup from "yup";
 import { Formik, Form } from "formik";
-import { Input, Textarea } from "neetoui/formik";
-import { Button } from "neetoui";
+import { Input, Textarea, Select, Switch } from "neetoui/formik";
+import { Button, DateInput } from "neetoui";
 import notesApi from "apis/notes";
 
+import { NOTE_TAGS } from "./constants";
+
 export default function NewNoteForm({ onClose, refetch }) {
+  const [dueDate, setDueDate] = useState(null);
+
   const handleSubmit = async values => {
-    try {
-      await notesApi.create(values);
-      refetch();
-      onClose();
-    } catch (err) {
-      logger.error(err);
-    }
-  };
+      try {
+        await notesApi.create(values);
+        refetch();
+        onClose();
+      } catch (err) {
+        logger.error(err);
+      }
+    },
+    handleDueDateChange = selectedDate => {
+      setDueDate(selectedDate);
+    },
+    contactsList = [
+      { label: "Mohan Singh", value: 1 },
+      { label: "Rohan Singh", value: 2 },
+      { label: "Sunil Kumar", value: 3 },
+      { label: "Mamta Verma", value: 4 },
+    ];
+
   return (
     <Formik
       initialValues={{
         title: "",
         description: "",
+        tag: "",
+        contact: null,
+        add_due_date: false,
+        due_date: dueDate,
       }}
       onSubmit={handleSubmit}
       validationSchema={yup.object({
@@ -27,10 +45,42 @@ export default function NewNoteForm({ onClose, refetch }) {
         description: yup.string().required("Description is required"),
       })}
     >
-      {({ isSubmitting }) => (
+      {({ values, isSubmitting }) => (
         <Form>
-          <Input label="Title" name="title" className="mb-6" />
-          <Textarea label="Description" name="description" rows={8} />
+          <Input label="Title" name="title" className="mb-4" />
+          <Select
+            name="tag"
+            label="Tags"
+            placeholder="Select a Tag"
+            options={NOTE_TAGS}
+            className="mb-4"
+          />
+          <Textarea
+            label="Description"
+            name="description"
+            rows={4}
+            className="mb-6"
+          />
+          <Select
+            name="contact"
+            label="Assigned Contact"
+            placeholder="Select a Contact"
+            options={contactsList}
+            className="mb-6"
+          />
+          <Switch
+            name="add_due_date"
+            label="Add Due Date to Note"
+            className="mb-4"
+          />
+          {values.add_due_date && (
+            <DateInput
+              label="Due Date"
+              placeholder="Select a Date"
+              value={dueDate}
+              onChange={handleDueDateChange}
+            />
+          )}
           <div className="nui-pane__footer nui-pane__footer--absolute">
             <Button
               onClick={onClose}
--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -2,3 +2,10 @@ export const DELETE_NOTE_MSG =
   "Are you sure you want to delete the note ? All of your data will be permanently removed from our database forever. This action cannot be undone.";
 export const DELETE_MULTIPLE_NOTES_MSG =
   "Are you sure you want to continue? This cannot be undone.";
+export const NOTE_TAGS = [
+  { label: "E Learning", value: "e learning" },
+  { label: "Tutorial", value: "tutorial" },
+  { label: "Internal", value: "internal" },
+  { label: "External", value: "external" },
+  { label: "Miscellaneous", value: "miscellaneous" },
+];
