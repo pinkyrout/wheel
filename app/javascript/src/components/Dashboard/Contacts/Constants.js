a8c9ecc5a9e555f14c4fcff1989f8717124f5dbe
Pinky Rout <pinkyrout10@gmail.com>
2021-06-10 15:51:19 +0530

    [5-new-contact-page-ui] 1. Added pane for contact create form. 2. Added new contact form and added constant file for the required constants.

--- app/javascript/src/components/Dashboard/Contacts/Constants.js
+++ app/javascript/src/components/Dashboard/Contacts/Constants.js
@@ -1,2 +1,26 @@
+import * as yup from "yup";
+
 export const DELETE_CONTACT_ALERT_MSG =
   "Are you sure you want to delete the contact ? All of your data will be permanently removed from our database forever. This action cannot be undone.";
+export const CONTACT_FORM_INITIAL_VALUES = {
+  name: "",
+  contactNumber: null,
+  department: "",
+  email: "",
+  addToBasecamp: false,
+};
+export const DEPARTMENTS = [
+  { label: "Engineering", value: "Engineering" },
+  { label: "Electronics", value: "Electronics" },
+  { label: "Biology", value: "Biology" },
+  { label: "Mechanical", value: "Mechanical" },
+];
+
+export const contactFormValidationSchema = yup.object({
+  name: yup.string().required("Name is required"),
+  email: yup.string().required("Email is required"),
+  contactNumber: yup
+    .number()
+    .transform(value => (isNaN(value) ? undefined : value))
+    .required("Contact Number is required"),
+});
---
+++ app/javascript/src/components/Dashboard/Contacts/NewContactPane.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Pane } from "neetoui";
+import NewContactForm from "./NewContactForm";
+
+export default function NewContactPane({ showPane, setShowPane }) {
+  const onClose = () => setShowPane(false);
+  return (
+    <Pane title="Create a New Contact" isOpen={showPane} onClose={onClose}>
+      <div className="px-6">
+        <NewContactForm onClose={onClose} />
+      </div>
+    </Pane>
+  );
+}
